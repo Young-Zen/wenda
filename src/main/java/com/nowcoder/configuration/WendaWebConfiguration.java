d76c775b5a74e61a9f7fc28add4a7b8af1e36c74
yanghj <2453883990@qq.com>
2019-09-30 14:20:17 +0800

    解决过时的类WebMvcConfigurerAdapter

--- src/main/java/com/nowcoder/configuration/WendaWebConfiguration.java
+++ src/main/java/com/nowcoder/configuration/WendaWebConfiguration.java
@@ -5,10 +5,10 @@ import com.nowcoder.interceptor.PassportInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
-public class WendaWebConfiguration extends WebMvcConfigurerAdapter {
+public class WendaWebConfiguration implements WebMvcConfigurer {
     @Autowired
     PassportInterceptor passportInterceptor;
 
@@ -19,6 +19,5 @@ public class WendaWebConfiguration extends WebMvcConfigurerAdapter {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(passportInterceptor);
         registry.addInterceptor(loginRequiredInterceptor).addPathPatterns("/user/*");
-        super.addInterceptors(registry);
     }
 }
